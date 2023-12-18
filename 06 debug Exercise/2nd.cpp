a07375305add482330bad42e0caba540e850c099
iamrahulmeena <rahulmeenaoffical@gmail.com>
2023-12-18 23:39:56 +0530

    pattren

--- 06 debug Exercise/2nd.cpp
+++ 06 debug Exercise/2nd.cpp
@@ -1,25 +1,19 @@
-/*
-Pattern
-1
-23
-345
-4567
-*/
 #include<iostream>
 using namespace std;
 
 
 int main(){
-    int n=6;
-    int i=0;
-    while(i<n){
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
         int j = i,count = 1;
-        while(count<i){
+        while(count<=i){
             cout<<j;
             j = j + 1;
             count = count + 1;
         }
         cout<<"\n";
-        // i = i + 1;
+        i = i + 1;
     }
 }
