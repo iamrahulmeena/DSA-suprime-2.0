b908a09cc02bd775f6922f5dbae68751fe84fba6
iamrahulmeena <rahulmeenaoffical@gmail.com>
2024-01-03 16:18:49 +0530

    class 9&10 done

---
+++ 10. Type casting/explicit.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a=5;
+    float b=2.6;
+
+    cout<<a+(int)b;
+}
---
+++ 10. Type casting/implicittypecasting.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a=90;
+    char ch=a;
+
+    cout<<ch;
+}
